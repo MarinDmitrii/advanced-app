83135dfcff6a7aec970fd646af8c7861e93ac6b3
vkr666 <vkr666@gmail.com>
2023-01-24 19:54:00 +0400

    add /POST endpoint json body & log

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/vkr666/advanced-app
 
 go 1.19
 
-require github.com/klyve/go-healthz v0.0.0-20190408055138-fd2dad35640e // indirect
+require (
+	github.com/klyve/go-healthz v0.0.0-20190408055138-fd2dad35640e // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"net/http/httputil"
+	"os"
 )
 
-func main() {
-	http.HandleFunc("/hello", Hello)
-	http.HandleFunc("/healthz", Healthz)
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Failed to create logfile")
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+	log.SetLevel(log.InfoLevel)
+	//log.SetReportCaller(true) //добавляет номер строки
+}
 
+func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/user", createUser)
+
+	makeHTTPserver()
+}
+
+func makeHTTPserver() {
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
+		log.Fatal(err)
 		panic(err)
 	}
 }
@@ -18,10 +40,76 @@ func main() {
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("It's alive!"))
+		_, err := w.Write([]byte("It's alive!"))
+		if err != nil {
+			return
+		}
 	}
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Master!")
+type user struct {
+	Name  string `json:"Name"`
+	Email string `json:"Email"`
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	headerContentType := r.Header.Get("Content-Type")
+	if headerContentType != "application/json" {
+		errorResponse(w, "Content Type is not application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+	var e user
+	var unmarshalErr *json.UnmarshalTypeError
+
+	//body, err := io.ReadAll(r.Body)
+	//json.Unmarshal(body, &e)
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&e)
+
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			errorResponse(w, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
+		} else {
+			errorResponse(w, "Bad Request "+err.Error(), http.StatusBadRequest)
+		}
+		return
+	}
+	userResponse(w, e, http.StatusOK)
+	//log.Printf("%s", r.RemoteAddr)
+	//log.Printf("%s", r.Header)
+
+	reqDump, err1 := httputil.DumpRequest(r, true)
+	if err1 != nil {
+		log.Fatalf("%s", err1)
+	}
+	//log.Debug(string(reqDump))
+	log.Print(string(reqDump))
+
+	return
+}
+
+func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	resp := make(map[string]string)
+	resp["message"] = message
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+	log.Printf("%s", jsonResp)
+}
+
+func userResponse(w http.ResponseWriter, e user, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	jsonUser, err := json.Marshal(&e)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonUser)
+	log.Printf("%s", jsonUser)
 }
